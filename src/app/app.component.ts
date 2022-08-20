0832670ece5ccedf4600bdc0e7ee54180a6320a9
Jerry Ebikari Needam <71222695+jerry-ebikari@users.noreply.github.com>
2022-08-20 21:07:14 +0100

    Sidebar
    Designed sidebar and added router links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,33 @@ export class AppComponent implements OnInit {
   isVisible = true;
   transition: any = 'uncover';
   isDimmedWhenVisible = false;
+  direction: any = 'left'
+  sidebarStyles: any = {
+    margin: 0,
+    padding: 0,
+    outline: 0
+  }
+  sidebarSiblingStyles: any = {
+    'overflow-y': 'auto',
+    'max-height': '100%',
+    'background-color': 'var(--navy)'
+  }
 
   ngOnInit(): void {
     this.sidebarSetup()
+    this.setSidebarAndContentWidth()
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.sidebarSetup()
+    this.setSidebarAndContentWidth()
   } 
+
+  // CHECK SCREEN WIDTH
+  isMobile() {
+    return window.innerWidth < 768;
+  }
   
   // SET SIDEBAR DIMENSIONS
   sidebarSetup() {
@@ -25,11 +43,50 @@ export class AppComponent implements OnInit {
       this.transition = 'overlay';
       this.isVisible = false;
       this.isDimmedWhenVisible = true;
+      this.direction = 'right';
     }
     else {
       this.transition = 'uncover';
       this.isVisible = true;
       this.isDimmedWhenVisible = false;
+      this.direction = 'left';
+    }
+  }
+
+  // SET SIDEBAR AND CONTENT WIDTH
+  setSidebarAndContentWidth() {
+    if (window.innerWidth > 768) {
+      this.sidebarStyles.width = '20%';
+      this.sidebarStyles.maxWidth = '236px';
+      this.sidebarSiblingStyles.width = window.innerWidth < 1180 ? '80%' : `${window.innerWidth - 236}px`;
+    }
+    else {
+      this.sidebarStyles.width ? delete this.sidebarStyles.width : '';
+      this.sidebarSiblingStyles.width = '100%';
+    }
+  }
+
+  // FIX CONTENT GOING OFF SCREEN
+  resize(sidebar: any) {
+    if (!this.isMobile()) {
+      sidebar.close();
+      sidebar.open();
+    }
+  }
+
+  // CLOSE SIDEBAR
+  close() {
+    this.isVisible = false;
+  }
+
+  // OPEN SIDEBAR
+  open() {
+    this.isVisible = true;
+  }
+
+  navlinkClicked() {
+    if (this.isMobile()) {
+      this.close();
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { SuiModule } from '@richardlt/ng2-semantic-ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
